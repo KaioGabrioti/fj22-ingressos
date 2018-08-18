7de11de83b6f13e083df09b651556d3c630a5faf
kaio.gabrioti <kgabrioti@gmail.com>
2018-08-18 12:05:22 -0300

    Inserindo Sessao

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,16 +2,41 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.Entity;
+
+@Entity
 public class Sessao {
 	
+	@Id
+	@GeneratedValue
+	private Long id;
+	@ManyToOne
 	private Sala sala;
-	
+	@ManyToOne
 	private Filme filme;
 	
 	private LocalTime horario;
 
-	public Sessao(){
-		
+	public Sessao(LocalTime horario, Filme filme, Sala sala){
+		this.horario = horario;
+		this.filme = filme;
+		this.sala = sala;
+	}
+	
+	public LocalTime getHorarioTermino(){
+		return this.horario.plusMinutes(filme.getDuracao().toMinutes());
+	}
+	
+	public Long getId(){
+		return id;
+	}
+	
+	public void setId(Long id){
+		this.id = id;
 	}
 
 	public Sala getSala() {
